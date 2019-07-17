dc887a99ba578ab14899ebccd216232243dcd503
anhtus <vuanhtu1993mta@gmail.com>
2019-07-17 10:15:09 -0700

    config

--- src/src/helpers/Constants.ts
+++ src/src/helpers/Constants.ts
@@ -1,8 +1,9 @@
 export class Constants {
-  public static stsAuthority = 'https://demo.identityserver.io/';
-  public static clientId = 'spa';
-  public static clientRoot = 'http://localhost:4200/';
-  public static clientScope = 'openid profile email api';
-
-  public static apiRoot = 'https://demo.identityserver.io/api/';
+  public static stsAuthority = 'http://10.151.137.228:5000';
+  public static clientId = 'cscorespa';
+  public static clientRoot = 'http://10.151.137.221:5100';
+  public static clientScope = 'openid profile CSCoreApi';
+  public static response_type = 'token id_token';
+  public static scope = 'openid profile CSCoreApi';
+  public static apiRoot = 'http://10.151.137.228:5200';
 }
--- src/src/services/AuthService.ts
+++ src/src/services/AuthService.ts
@@ -1,40 +1,42 @@
-import { Log, User, UserManager } from 'oidc-client';
+import {Log, User, UserManager} from 'oidc-client';
 
-import { Constants } from '../helpers/Constants';
+import {Constants} from '../helpers/Constants';
 
 export class AuthService {
-  public userManager: UserManager;
+    public userManager: UserManager;
 
-  constructor() {
-    const settings = {
-      authority: Constants.stsAuthority,
-      client_id: Constants.clientId,
-      redirect_uri: `${Constants.clientRoot}signin-callback.html`,
-      silent_redirect_uri: `${Constants.clientRoot}silent-renew.html`,
-      // tslint:disable-next-line:object-literal-sort-keys
-      post_logout_redirect_uri: `${Constants.clientRoot}`,
-      response_type: 'code',
-      scope: Constants.clientScope
-    };
-    this.userManager = new UserManager(settings);
+    constructor() {
+        const settings = {
+            client_id: 'cscorespa',
+            redirect_uri: 'http://10.151.137.221:5100/#/callback',
+            response_type: 'token id_token',
+            scope: "openid profile CSCoreApi",
+            authority: 'http://10.151.137.228:5000',
+            silent_redirect_uri: 'http://10.151.137.221:5100/silentRenew.html',
+            automaticSilentRenew: true,
+            filterProtocolClaims: true,
+            loadUserInfo: true,
+            monitorSession: true
+        };
+        this.userManager = new UserManager(settings);
 
-    Log.logger = console;
-    Log.level = Log.INFO;
-  }
+        Log.logger = console;
+        Log.level = Log.INFO;
+    }
 
-  public getUser(): Promise<User | null> {
-    return this.userManager.getUser();
-  }
+    public getUser(): Promise<User | null> {
+        return this.userManager.getUser();
+    }
 
-  public login(): Promise<void> {
-    return this.userManager.signinRedirect();
-  }
+    public login(): Promise<void> {
+        return this.userManager.signinRedirect();
+    }
 
-  public renewToken(): Promise<User> {
-    return this.userManager.signinSilent();
-  }
+    public renewToken(): Promise<User> {
+        return this.userManager.signinSilent();
+    }
 
-  public logout(): Promise<void> {
-    return this.userManager.signoutRedirect();
-  }
+    public logout(): Promise<void> {
+        return this.userManager.signoutRedirect();
+    }
 }
